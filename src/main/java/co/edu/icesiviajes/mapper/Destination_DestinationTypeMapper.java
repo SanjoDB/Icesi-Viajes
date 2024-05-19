e490573210d2f869c919c166c905eeabe8ad9550
SanjoDB <1006110438@u.icesi.edu.co>
2024-05-19 01:35:23 -0500

    ID reference fixed

--- src/main/java/co/edu/icesiviajes/mapper/Destination_DestinationTypeMapper.java
+++ src/main/java/co/edu/icesiviajes/mapper/Destination_DestinationTypeMapper.java
@@ -11,19 +11,19 @@ import java.util.List;
 public interface Destination_DestinationTypeMapper {
 
     @Mapping(source = "destination.id_destination", target = "destination")
-    @Mapping(source = "destinationType.id_desType", target = "destinationType")
+    @Mapping(source = "destinationType.idDesType", target = "destinationType")
     Destination_DestinationTypeDTO toDestination_DestinationTypeDTO(Destination_DestinationType entity);
 
     @Mapping(source = "destination", target = "destination.id_destination")
-    @Mapping(source = "destinationType", target = "destinationType.id_desType")
+    @Mapping(source = "destinationType", target = "destinationType.idDesType")
     Destination_DestinationType toDestination_DestinationType(Destination_DestinationTypeDTO entity);
 
     @Mapping(source = "destination.id_destination", target = "destination")
-    @Mapping(source = "destinationType.id_desType", target = "destinationType")
+    @Mapping(source = "destinationType.idDesType", target = "destinationType")
     List<Destination_DestinationTypeDTO> toDestination_DestinationTypeDTO(List<Destination_DestinationType> entity);
 
     @Mapping(source = "destination", target = "destination.id_destination")
-    @Mapping(source = "destinationType", target = "destinationType.id_desType")
+    @Mapping(source = "destinationType", target = "destinationType.idDesType")
     List<Destination_DestinationType> toDestination_DestinationType(List<Destination_DestinationTypeDTO> entity);
 
 }
