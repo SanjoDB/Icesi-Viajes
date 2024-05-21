30d405209ed7f2d62df7ad8fc2e3b928ce52bb2b
Victor Manuel Garzon Meneses <panterrosa90@gmail.com>
2024-05-20 20:38:14 -0500

    Feat: JWT

---
+++ src/main/java/co/edu/icesiviajes/config/AuthConfig.java
@@ -0,0 +1,9 @@
+package co.edu.icesiviajes.config;
+
+import lombok.Data;
+
+@Data
+public class AuthConfig {
+    private String email;
+    private String password;
+}
--- src/main/java/co/edu/icesiviajes/config/SecurityConfig.java
+++ src/main/java/co/edu/icesiviajes/config/SecurityConfig.java
@@ -1,14 +1,65 @@
 package co.edu.icesiviajes.config;
 
+import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+
+@EnableWebSecurity
+@AllArgsConstructor
+@CrossOrigin
 @Configuration
 public class SecurityConfig {
+
+    private final UserDetailsService userDetailsService;
+    private final JWTAuthorizationFilter jwtAuthorizationFilter;
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http, AuthenticationManager authManager) throws Exception {
+        JWTAuthenticationFilter jwtAuthenticationFilter = new JWTAuthenticationFilter();
+        jwtAuthenticationFilter.setAuthenticationManager(authManager);
+        jwtAuthenticationFilter.setFilterProcessesUrl("/auth");
+
+
+        return http.
+                csrf().disable()
+                .cors(Customizer.withDefaults())
+                .authorizeHttpRequests(authRequest ->
+                        authRequest
+                                .requestMatchers("/auth", "api/v1/user/login","api/v1/user/save").permitAll()
+                                .anyRequest().authenticated()
+                )
+                .sessionManagement(sessionManager ->
+                        sessionManager
+                                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+                .addFilter(jwtAuthenticationFilter)
+                .addFilterBefore(jwtAuthorizationFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
+    }
+
+
+    @Bean
+    AuthenticationManager authManager(HttpSecurity http, PasswordEncoder passwordEncoder ) throws Exception {
+        return http.getSharedObject(AuthenticationManagerBuilder.class).userDetailsService(userDetailsService)
+                .passwordEncoder(passwordEncoder)
+                .and()
+                .build();
     }
 }
