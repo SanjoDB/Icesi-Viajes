e1c039c2d69a0c77bfa3a266b60e329bd3ae16f6
dylanbc1 <dyez1110@gmail.com>
2024-06-06 02:25:23 -0500

    Cors

--- src/main/java/co/edu/icesiviajes/controller/IdentificacionTypeRestController.java
+++ src/main/java/co/edu/icesiviajes/controller/IdentificacionTypeRestController.java
@@ -5,6 +5,7 @@ import co.edu.icesiviajes.service.IdentificationTypeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@CrossOrigin("http://localhost:5173")
 @RequestMapping("api/v1/identificationType")
 public class IdentificacionTypeRestController {
 
