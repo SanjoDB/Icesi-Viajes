68359042fceec9ef06b51eb75160fc7dcc2cfe5d
dylanbc1 <dyez1110@gmail.com>
2024-06-05 23:38:36 -0500

    Update PlanDTO.java

--- src/main/java/co/edu/icesiviajes/dto/PlanDTO.java
+++ src/main/java/co/edu/icesiviajes/dto/PlanDTO.java
@@ -14,10 +14,22 @@ public class PlanDTO {
     private Integer num_people;
     private Date start_Date;
     private Date end_Date;
+
+    public PlanDTO(String code, String name, String description, Integer num_people, Date start_Date, Date end_Date, Double price, String state, String image) {
+        this.code = code;
+        this.name = name;
+        this.description = description;
+        this.num_people = num_people;
+        this.start_Date = start_Date;
+        this.end_Date = end_Date;
+        this.price = price;
+        this.state = state;
+        this.image = image;
+    }
+
     private Double price;
     private String state;
-    private Integer client;
-    private Integer user;
     private String image;
+
 
 }
